aeba0e42e6add97d80c3bfa6d754b8035548bf5b
Tonye Greene <35413072+ellysmoore@users.noreply.github.com>
2021-06-15 14:27:15 +0100

    removed a member

--- src/Components/Teams.js
+++ src/Components/Teams.js
@@ -120,30 +120,6 @@ function Teams() {
             >
               <div className="member">
                 <div className="pic">
-                  <img src={abiodun} className="img-fluid" alt="" />
-                </div>
-                <div className="member-info">
-                  <h4>Abiodun Salami</h4>
-                  <span>Human Resources Head</span>
-                  <div className="social">
-                    <Link to="https://twitter.com/biodunn_">
-                      <i className="icofont-twitter"></i>
-                    </Link>
-                    <Link to="https://instagram.com/biodunn_">
-                      <i className="icofont-instagram"></i>
-                    </Link>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div
-              className="col-xl-3 col-lg-4 col-md-6"
-              data-aos="fade-up"
-              data-aos-delay="300"
-            >
-              <div className="member">
-                <div className="pic">
                   <img src={moyo} className="img-fluid" alt="" />
                 </div>
                 <div className="member-info">
